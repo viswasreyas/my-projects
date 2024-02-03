83a1d4294788998354d6398b2ea56437369662df
Viswasreyas <viswasreyasv@gmail.com>
2024-02-03 23:12:29 +0530

    created 2d board structure

--- TicTacToe/src/TicTacToe.java
+++ TicTacToe/src/TicTacToe.java
@@ -1,5 +1,13 @@
 public class TicTacToe  {
     public static void main(String[] args) {
+        int row = 3;
+        int col = 3;
+        char[][] board = new char[row][col];
 
+        for(int i = 0; i < row; i++) {
+            for(int j=0; j < col; j++) {
+                board[i][j] = ' ';
+            }
+        }
     }
 }
