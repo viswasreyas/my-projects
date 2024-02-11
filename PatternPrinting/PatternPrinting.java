3e10dbd91e3cbedb1383ea0fadcb50e8d3acea23
Viswasreyas <viswasreyasv@gmail.com>
2024-02-11 16:47:17 +0530

    Pattern1 logic added

---
+++ PatternPrinting/PatternPrinting.java
@@ -0,0 +1,24 @@
+public class PatternPrinting {
+    public static void main(String[] args) {
+        int n = 5;
+        printPattern1(n);
+    }
+/*
+ *
+ * *
+ * * *
+ * * * *
+ * * *
+ * *
+ *
+ */
+    private static void printPattern1(int n) {
+        for (int row = 0; row < 2 * n; row++) {
+            int noOfCols = (row > n) ? 2 * n - row : row;
+            for (int i = 0; i < noOfCols; i++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
