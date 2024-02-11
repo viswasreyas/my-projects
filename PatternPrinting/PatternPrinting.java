3e091c8ec35e1d07cf2cf827c45430918e7143e4
Viswasreyas <viswasreyasv@gmail.com>
2024-02-11 16:56:46 +0530

    Pattern2 logic added

--- PatternPrinting/PatternPrinting.java
+++ PatternPrinting/PatternPrinting.java
@@ -21,4 +21,31 @@ public class PatternPrinting {
             System.out.println();
         }
     }
+
+    /*
+          *
+         * *
+        * * *
+       * * * *
+      * * * * *
+     * * * * * *
+      * * * * *
+       * * * *
+        * * *
+         * *
+          *
+     */
+    private static void printPattern2(int n) {
+        for (int row = 0; row < 2 * n ; row++) {
+            int noOfSpaces = (row > n) ? row - n : n - row;
+            int patternPerRow = (row > n) ? 2 * n - row : row;
+            for (int i = 0; i < noOfSpaces; i++) {
+                System.out.print(" ");
+            }
+            for (int i = 0; i < patternPerRow; i++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
 }
